2e45d3a1d806b89a65299c775b7b7bc40a6b68b3
charan_999 <charandoppalapudi2002@gmail.com>
2024-09-04 15:55:31 +0530

    upload portfolio

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//Scroll Section
+let sections=querySelectorAll('section');
+let navLinks=querySelectorAll('header  nav a');
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top=window.scrollY;
+        let offset=sec.offsetTop -100;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id');
+        if(top >= offset && top < offset + height){
+            //active number links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            }
+        
+        
+    });
+    //stickyheader
+    let header=document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 100);
+}
